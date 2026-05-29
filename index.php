ef5458ee8185abcc1a54f464839fad70acc7a84d
Javi <javierdiazsoriano12@gmail.com>
2026-05-29 18:11:16 +0200

    Otro mas :(

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/app/modelo/funciones.php';
 require_once __DIR__ . '/vendor/autoload.php';
 // require_once '../config.htaccess';
 
-define('BASE_PATH', '.');
+define('BASE_PATH', './');
 define('LINKS_PATH', './public');
 define('VIEWS_BACK_PATH', '/../../app/vista');
 // define('VIEWS_PATH', '/app/vista');
